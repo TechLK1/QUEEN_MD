c31a822b5ef42dd43f91cb98b7c3dca2f9909856
CAMP-LK <ddilisha2005@gmail.com>
2025-02-23 08:48:02 +0530

    ➳ᴹᴿ᭄📌 𝙼𝚄𝙻𝚃𝙸-𝙳𝙴𝚅𝙸𝙲𝙴 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃 𝚂𝚁𝙸𝙻𝙰𝙽𝙺𝙰𝙽 𝙽𝙾1 𝙱𝙾𝚃 𝙼𝙰𝙵𝙸𝙰 📌 ꔪＯＰ ᭄ ☯࿐

--- plugins/alive.js
+++ plugins/alive.js
@@ -26,7 +26,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send the status message with an image
         await conn.sendMessage(from, { 
-            image: { url: `https://i.ibb.co/1gyyZ0M/temp-Img2-Url.jpg` },  // Image URL
+            image: { url: `https://i.ibb.co/fV8G0y1W/8352.jpg` },  // Image URL
             caption: status,
             contextInfo: {
                 mentionedJid: [m.sender],
