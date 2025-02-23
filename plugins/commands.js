142d1abf4860218b21773981784120f97982168b
CAMP-LK <ddilisha2005@gmail.com>
2025-02-23 08:49:50 +0530

    ➳ᴹᴿ᭄📌 𝙼𝚄𝙻𝚃𝙸-𝙳𝙴𝚅𝙸𝙲𝙴 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃 𝚂𝚁𝙸𝙻𝙰𝙽𝙺𝙰𝙽 𝙽𝙾1 𝙱𝙾𝚃 𝙼𝙰𝙵𝙸𝙰 📌 ꔪＯＰ ᭄ ☯࿐

--- plugins/commands.js
+++ plugins/commands.js
@@ -297,7 +297,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/YTZH7zLQ/temp-Img2-Url.jpg` },
+                image: { url: `https://i.ibb.co/zhwQm1RJ/temp-Img2-Url.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
